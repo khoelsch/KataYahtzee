f15cd875464f41d2c63a6f41f1e25032a9a066e4
Kai Hoelscher <Kai.Hoelscher@coremedia.com>
2011-08-17 16:18:09 +0200

    Initial project structure

---
+++ src/main/java/YahtzeeEvaluator.java
@@ -0,0 +1,11 @@
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Evaluates the roll of five six-sided dices in the context of the Yahtzee (german: 'Kniffel') game.
+ *
+ * @see <a href="http://codingdojo.org/cgi-bin/wiki.pl?KataYahtzee">Description of this Kata @CodingDojo</a>
+ */
+public class YahtzeeEvaluator {
+
+}
