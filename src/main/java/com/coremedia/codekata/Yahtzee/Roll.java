cff0d422640c04dd1b371da8b43bac96da7826a8
Kai Hoelscher <Kai.Hoelscher@coremedia.com>
2012-10-25 21:32:14 +0200

    Restart, ...again!

---
+++ src/main/java/com/coremedia/codekata/Yahtzee/Roll.java
@@ -0,0 +1,12 @@
+package com.coremedia.codekata.Yahtzee;
+
+/**
+ * <p>Implementation of <a href="http://codingdojo.org/cgi-bin/wiki.pl?KataYahtzee">KataYahtzee</a>.</p>
+ *
+ * <p>A dice roll in a game of yahtzee.</p>
+ */
+public final class Roll {
+  public void add(int numberOnDice) {
+
+  }
+}
---
+++ src/main/java/com/coremedia/codekata/Yahtzee/RollTest.java
@@ -0,0 +1,15 @@
+package com.coremedia.codekata.Yahtzee;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: khoelsch
+ * Date: 25.10.12
+ * Time: 21:27
+ * To change this template use File | Settings | File Templates.
+ */
+public class RollTest {
+  @org.junit.Test(expected = IllegalArgumentException.class)
+  public void testAdd() throws Exception {
+
+  }
+}
