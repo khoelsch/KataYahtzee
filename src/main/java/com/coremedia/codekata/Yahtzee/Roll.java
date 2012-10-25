6a21e114123cf9eba7a5006998bb094d272dcb9d
Kai Hoelscher <Kai.Hoelscher@coremedia.com>
2012-10-25 22:15:02 +0200

    Basic integrity checks

--- src/main/java/com/coremedia/codekata/Yahtzee/Roll.java
+++ src/main/java/com/coremedia/codekata/Yahtzee/Roll.java
@@ -1,12 +1,63 @@
 package com.coremedia.codekata.Yahtzee;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * <p>Implementation of <a href="http://codingdojo.org/cgi-bin/wiki.pl?KataYahtzee">KataYahtzee</a>.</p>
- *
+ * <p/>
  * <p>A dice roll in a game of yahtzee.</p>
  */
 public final class Roll {
-  public void add(int numberOnDice) {
+  private List<Integer> dicesRolled;
 
+  public Roll() {
+    dicesRolled = new ArrayList<Integer>();
+  }
+
+  /**
+   * Adds a dice to the roll.
+   *
+   * @param numberOnDice The number on the dice added to the roll
+   * @throws IllegalStateException if five dices have already been added to the roll
+   * @return the {@link Roll} we add the dice to
+   */
+  public Roll add(int numberOnDice) {
+    if (numberOnDice <= 0 || numberOnDice > 6) {
+      throw new IllegalArgumentException("'" + numberOnDice + "' is an invalid dice number.");
+    }
+
+    if (dicesRolled.size() == 5) {
+      throw new IllegalStateException("Five dices have already been added to the roll.");
+    }
+
+    dicesRolled.add(Integer.valueOf(numberOnDice));
+    return this;
+  }
+
+  /**
+   * Evaluates the whole dice roll.
+   *
+   * @return the total of points this roll would evaluate to in a game of Yahtzee
+   * @throws IllegalStateException if an insufficient number of dice has been added to the roll
+   */
+  public int eval(Rule rule) {
+    if (dicesRolled.size() < 5) {
+      throw new IllegalStateException("an insufficient number of dice has been added to the roll");
+    }
+
+    return -1;
+  }
+
+  interface Rule {
+    int apply();
+  }
+
+  static class DicesHaveTheSameNumberRule implements Rule {
+    private int diceNumber;
+    DicesHaveTheSameNumberRule(int diceNumber) { this.diceNumber = diceNumber; }
+     public int apply() {
+      return 0;  //To change body of implemented methods use File | Settings | File Templates.
+    }
   }
 }
--- src/main/java/com/coremedia/codekata/Yahtzee/RollTest.java
+++ src/main/java/com/coremedia/codekata/Yahtzee/RollTest.java
@@ -1,5 +1,7 @@
 package com.coremedia.codekata.Yahtzee;
 
+import org.junit.Test;
+
 /**
  * Created with IntelliJ IDEA.
  * User: khoelsch
@@ -8,8 +10,25 @@ package com.coremedia.codekata.Yahtzee;
  * To change this template use File | Settings | File Templates.
  */
 public class RollTest {
-  @org.junit.Test(expected = IllegalArgumentException.class)
-  public void testAdd() throws Exception {
-
+  @Test(expected = IllegalArgumentException.class)
+  public void add_lessThanOne() throws Exception {
+    new Roll().add(0);
   }
+
+
+  @Test(expected = IllegalArgumentException.class)
+  public void add_greaterThanSix() throws Exception {
+    new Roll().add(7);
+  }
+
+  @Test(expected = IllegalStateException.class)
+  public void add_tooManyDiceRolls() {
+    new Roll().add(1).add(2).add(3).add(4).add(5).add(6);
+  }
+
+  @Test(expected = IllegalStateException.class)
+  public void eval_insufficientDicesAdded() {
+    new Roll().add(1).add(2).add(3).add(4).eval(new Roll.DicesHaveTheSameNumberRule(1)); // Rule type does not matter here
+  }
+
 }
